dfefc047a2e50d45c0d21f2c2e3e9bd4f4660f9f
jsculsp <328232234@qq.com>
2017-06-19 17:40:04 +0800

    save

--- controller.js
+++ controller.js
@@ -7,25 +7,21 @@ const log = console.log.bind(console)
 let apiGet = (url, router, mapping) => {
     let path = url.substring(4)
     router.get(path, mapping[url])
-    log(`register URL mapping: GET ${path}`)
 }
 
 let apiPost = (url, router, mapping) => {
     let path = url.substring(5)
     router.post(path, mapping[url])
-    log(`register URL mapping: POST ${path}`)
 }
 
 let apiPut = (url, router, mapping) => {
     let path = url.substring(4)
     router.put(path, mapping[url])
-    log(`register URL mapping: PUT ${path}`)
 }
 
 let apiDelete = (url, router, mapping) => {
     let path = url.substring(7)
     router.del(path, mapping[url])
-    log(`register URL mapping: DELETE ${path}`)
 }
 
 let apiOthers = (url, router, mapping) => {
@@ -55,7 +51,6 @@ let addControllers = (router, dir) => {
     let fileAll = fs.readdirSync(`${__dirname}/${dir}`)
     let fileFiltered = fileAll.filter(f => f.endsWith('.js'))
     fileFiltered.forEach(f => {
-        log(`process controller: ${f}...`)
         let mapping = require(`${__dirname}/${dir}/${f}`)
         addMapping(router, mapping)
     })
